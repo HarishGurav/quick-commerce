556f503a823727c48b908d5212b4889e9b4a4bdb
unknown <harishgurav930@gmail.com>
2024-06-22 20:24:02 +0530

    new comit

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { CartComponent } from '../../components/cart/cart.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CartComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  providers:[BsModalService]
 })
 export class HeaderComponent {
+  modalRef?: BsModalRef;
+
+  constructor(private bsModalService:BsModalService){
+
+
+  }
+
+  openCart(){
+    this.modalRef=this.bsModalService.show(CartComponent)
+
+  }
 
 }
